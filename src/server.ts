b0d79aa59f8d81c8d166b59b12c9cc18e4ae2601
Alícia Foureaux <aliciafoureauxlucas@gmail.com>
2020-10-23 17:51:40 -0300

    Create connection with database and add routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+import './database/connection';
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
     console.log('Server started!');
