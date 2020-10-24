92689178d715c2f409ce139402bd85ca54b68cf8
Alícia Foureaux <aliciafoureauxlucas@gmail.com>
2020-10-24 01:23:24 -0300

    Hash password when create user

--- src/modules/user/services/CreateUserService.ts
+++ src/modules/user/services/CreateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 
 import User from '../infra/typeorm/entities/User';
+import BCryptHashProvider from '../providers/HashProvider/implementations/BCryptHashProvider';
 
 interface Request {
     name: string;
@@ -11,8 +12,11 @@ interface Request {
 class CreateUserService {
     public async execute({ name, email, password }: Request): Promise<User> {
         const usersRepository = getRepository(User);
+        const hashProvider = new BCryptHashProvider();
 
-        const user = usersRepository.create({ name, email, password });
+        const hashedPassword = await hashProvider.generateHash(password);
+
+        const user = usersRepository.create({ name, email, password: hashedPassword });
 
         await usersRepository.save(user);
 
